64efb8d45524f334c63f1947f93ee2cc9481a298
Eugene Molotov <molotov@it-projects.info>
2023-06-08 11:09:16 +0500

    [IMP] web_last_viewed_records: black, isort, prettier

---
+++ web_last_viewed_records/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Last viewed records",
+    "version": "15.0.1.0.0",
+    "author": "Ivan Yelizariev, Odoo Community Association (OCA)",
+    "category": "Base",
+    "website": "https://github.com/OCA/web",
+    "depends": ["web", "mail"],
+    "data": [
+        "views.xml",
+    ],
+    "qweb": [
+        "static/src/xml/main.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
